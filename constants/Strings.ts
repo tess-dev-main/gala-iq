2b28edb3fc56d72c42eeeb9184b947afca4b2dec
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-02-24 14:00:27 -0600

    Rename UrlGenerator to EndpointWithId

--- constants/Strings.ts
+++ constants/Strings.ts
@@ -1,8 +1,8 @@
-export interface UrlGenerator {
+export interface EndpointWithId {
     (id: number): string;
 } 
 
 export const getUsers: string = "https://jsonplaceholder.typicode.com/users";
 export const getPhotos: string = "https://jsonplaceholder.typicode.com/photos";
-export const getUserAlbums: UrlGenerator = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
-export const getAlbumById: UrlGenerator = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
+export const getUserAlbums: EndpointWithId = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
+export const getAlbumById: EndpointWithId = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
