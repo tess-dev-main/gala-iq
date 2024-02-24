fa4c60aeb6fd54ab970b20940b761c64a09fc38b
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-02-24 12:29:47 -0600

    Clarified variable names

--- constants/Strings.ts
+++ constants/Strings.ts
@@ -4,5 +4,5 @@ export interface UrlGenerator {
 
 export const getUsers: string = "https://jsonplaceholder.typicode.com/users";
 export const getPhotos: string = "https://jsonplaceholder.typicode.com/photos";
-export const getAlbums: UrlGenerator = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
-export const getAlbum: UrlGenerator = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
+export const getUserAlbums: UrlGenerator = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
+export const getAlbumById: UrlGenerator = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
