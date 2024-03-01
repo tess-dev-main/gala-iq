78280c9f0e39892c294c6a4e81e898cd1a83175b
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-02-29 23:32:39 -0600

    Add BLUR_HASH to Strings

--- constants/Strings.ts
+++ constants/Strings.ts
@@ -6,3 +6,4 @@ export const getUsers: string = "https://jsonplaceholder.typicode.com/users";
 export const getPhotos: string = "https://jsonplaceholder.typicode.com/photos";
 export const getUserAlbums: EndpointWithId = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
 export const getAlbumById: EndpointWithId = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
+export const BLUR_HASH: string = "UKO2:N%2Tw=w]~RBVZRi};RPxuwHtLOtxZ%g";
