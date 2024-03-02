fc0817bdb52a308b0ef7697f7d27a04b40413114
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-03-02 01:32:42 -0600

    Add fetchUserAlbums thunk

--- features/userSlice.ts
+++ features/userSlice.ts
@@ -1,11 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-import { getUsers } from '../constants/Strings';
-import { User, UserState } from '../interfaces';
+import { getUserAlbums, getUsers } from '../constants/Strings';
+import { Album, FetchAlbumsResponse, User, UserState } from '../interfaces';
+import { useAppDispatch } from "@/hooks";
 
 export const fetchUsers = createAsyncThunk<User[]>('user/fetchUsers', () => {
-    return axios.get(getUsers).then(res => res.data)
+    return axios.get(getUsers).then(res => res.data);
+});
+
+export const fetchUserAlbums = createAsyncThunk<User[], User[]>('user/fetchAlbums', async (users: User[]) => {
+    const updatedUsers = await Promise.all(users.map(async (user) => {
+        const albums: Album[] = await axios.get(getUserAlbums(user.id)).then(res => res.data);
+        return {...user, albums};
+    }))
+
+    return updatedUsers;
 });
 
 const initialState: UserState = {
@@ -29,6 +39,18 @@ const userSlice = createSlice({
                 state.users = action.payload;
             })
             .addCase(fetchUsers.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message ? action.error.message : 'Error fetching users, please try again.';
+            })
+            .addCase(fetchUserAlbums.pending, (state) => {
+                state.loading = true;
+                state.error = '';
+            })
+            .addCase(fetchUserAlbums.fulfilled, (state, action) => {
+                state.loading = false;
+                state.users = action.payload;
+            })
+            .addCase(fetchUserAlbums.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.error.message ? action.error.message : 'Error fetching users, please try again.';
             })
