4a94fa4ac7e71d4fa77bf19cdda3184846fb97d9
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-03-05 07:00:21 -0600

    Add string func for getAlbumAsyncAddress; update interface

--- constants/Strings.ts
+++ constants/Strings.ts
@@ -1,9 +1,10 @@
-export interface EndpointWithId {
+export interface StringWithId {
     (id: number): string;
 } 
 
 export const getUsers: string = "https://jsonplaceholder.typicode.com/users";
 export const getPhotos: string = "https://jsonplaceholder.typicode.com/photos";
-export const getUserAlbums: EndpointWithId = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
-export const getAlbumById: EndpointWithId = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
+export const getUserAlbums: StringWithId = (userId) => `https://jsonplaceholder.typicode.com/albums?userId=${userId}`;
+export const getAlbumById: StringWithId = (albumId) => `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`;
 export const BLUR_HASH: string = "UKO2:N%2Tw=w]~RBVZRi};RPxuwHtLOtxZ%g";
+export const getAlbumAsyncAddress: StringWithId = (albumId) => `album_${albumId}`;
