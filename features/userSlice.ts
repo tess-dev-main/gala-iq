af7dce9ee0bdf391cebf34e3ad19d8fef165d3ca
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-03-05 05:06:08 -0600

    Add logic to check AsyncStorage for album; save albums to AsyncStorage

--- features/userSlice.ts
+++ features/userSlice.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { getUserAlbums, getUsers } from '../constants/Strings';
 import { Album, FetchAlbumsResponse, User, UserState } from '../interfaces';
 import { useAppDispatch } from "@/hooks";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const fetchUsers = createAsyncThunk<User[]>('user/fetchUsers', () => {
     return axios.get(getUsers).then(res => res.data);
@@ -11,8 +12,27 @@ export const fetchUsers = createAsyncThunk<User[]>('user/fetchUsers', () => {
 
 export const fetchUserAlbums = createAsyncThunk<User[], User[]>('user/fetchAlbums', async (users: User[]) => {
     const updatedUsers = await Promise.all(users.map(async (user) => {
-        const albums: Album[] = await axios.get(getUserAlbums(user.id)).then(res => res.data);
-        return {...user, albums};
+        const res: Album[] = [];
+        const data: Album[] = await axios.get(getUserAlbums(user.id)).then(res => res.data);
+
+        for (const album of data) {
+            try {
+                const _albumFromStorage = await AsyncStorage.getItem(`album_${album.id}`);
+                let albumFromStorage: Album | null = null;
+
+                if (!_albumFromStorage) {
+                    album.isArchived = false;
+                    await AsyncStorage.setItem(`album_${album.id}`, JSON.stringify(album));
+                } else {
+                    albumFromStorage = JSON.parse(_albumFromStorage);
+                }
+
+                if (albumFromStorage && !albumFromStorage?.isArchived) res.push(albumFromStorage);
+            } catch (error) {
+                throw error;
+            }
+        }
+        return {...user, albums: res};
     }))
 
     return updatedUsers;
