cd387aedc5a75057a39d522460bdd03468e8088a
tess-dev-main <110800399+tess-dev-main@users.noreply.github.com>
2024-03-06 04:48:40 -0600

    Remove header from index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -52,6 +52,7 @@ function RootLayoutNav() {
     <Provider store={store}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }}/>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
